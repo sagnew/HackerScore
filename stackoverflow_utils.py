71e8dbfa00e7a491a64a2ad670f45cdae0a4f950
Sam Agnew <sagnew@eden.rutgers.edu>
2013-09-21 23:27:40 -0400

    Scrape SO reputation

---
+++ stackoverflow_utils.py
@@ -0,0 +1,11 @@
+import requests
+import simplejson
+import random
+
+
+def get_reputation(user_id):
+    url = "http://api.stackoverflow.com/1.1/users/"
+    r = requests.get(url + user_id)
+    return r.json['users'][0]['reputation']
+
+print get_reputation('385913')
